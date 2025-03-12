894fc20f8804757d9c10fd563ad192d552c4654c
basscraft <cloudning@gmail.com>
2025-03-13 03:29:46 +0900

    Remove: 불필요한 테스트 메서드(초기 테스트용) 삭제

--- src/main/java/batch/mapper/ServerAuditMapper.java
+++ src/main/java/batch/mapper/ServerAuditMapper.java
@@ -7,8 +7,6 @@ import java.util.Map;
 
 @Mapper
 public interface ServerAuditMapper {
-    List<Map<String,String>> selectTest(int seq);
-
     Map<String,Object> selectOneServerAuditMasterByConnectionId(Map<String, Object> param);
     List<Map<String,Object>> selectPrivatePolicyHistByConnectionId(Map<String, Object> param);
     Map<String,Object> selectOneIncompleteQueryByConnectionId(Map<String, Object> param);
